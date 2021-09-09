f44e2f3bc6f5e55a58752c6a6f8a913b59b715b9
ThePanToster <73584389+ThePanToster@users.noreply.github.com>
2021-09-09 09:34:42 +0200

    yy

---
+++ HTML CSS JS/JS/25.js
@@ -0,0 +1,9 @@
+wynik = document.getElementById("wynik");
+document.getElementById("przycisk").addEventListener("click", losuj);
+
+function losuj(){
+	input = document.getElementById("ilosc");
+	for(){
+
+	}
+}
