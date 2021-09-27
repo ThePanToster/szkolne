67f5cc5ad2919aa9d5b2dc2b3cca3c6318492538
ThePanToster <73584389+ThePanToster@users.noreply.github.com>
2021-09-27 15:21:33 +0200

    usun

--- HTML CSS JS/JS27/27.js
+++
@@ -1,9 +0,0 @@
-for(i=1;i<=4;i++){
-    document.getElementById("d"+i).addEventListener("click", klik);
-    document.getElementById("d"+i).addEventListener("mouseover", hower);
-    document.getElementById("d"+i).addEventListener("mouseout", oucik);
-}
-
-function klik(){    window.alert("Kliknąłeś przycisk "+this.id);    }
-function hower(){   this.style.borderColor = "grey gainsboro gainsboro grey";   }
-function oucik(){   this.style.borderColor = "gainsboro grey grey gainsboro";   }
