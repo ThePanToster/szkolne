44717cd1ac7dc760ce43128c75d6f4e59dae4e24
ThePanToster <73584389+ThePanToster@users.noreply.github.com>
2021-10-25 10:06:11 +0200

    zmiany

--- PHP/8/save_user.php
+++ PHP/8/save_user.php
@@ -11,11 +11,24 @@
             $l = mysqli_real_escape_string($connection, $l);
             $n = mysqli_real_escape_string($connection, $n);
 
-            $query = "INSERT INTO users values('','$n','$l','$p')";
+            $query = "SELECT user_login, user_password FROM users WHERE user_login='{$l}'";
             $result = mysqli_query($connection, $query);
 
-            if($result) header("Location:index.php?status=ok");
-            else header("Location:index.php?status=error");
+            if($result){
+                if (mysqli_num_rows($result) == 0){
+                    $query = "INSERT INTO users values('','$n','$l','$p')";
+                    $result = mysqli_query($connection, $query);
+                    
+                    if($result) header("Location:index.php?status=ok");
+                    else header("Location:index.php?status=error");
+                }
+                else{
+                    echo "Konto o takim loginie już istnieje";
+                }
+            }
+            else {
+                echo "Błąd połączenia";
+            }
         }
         else{
             header("Location:index.php?status=empty");
