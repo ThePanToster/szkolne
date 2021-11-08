af4f97b59e05408fb5b9eb0c5e28ffc2a4420382
ThePanToster <alcrafter@o2.pl>
2021-11-08 13:50:56 +0100

    Projekt add

---
+++ PHP/projekt/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Projekt na AI :D">
+    <meta name="keywords" content="html, css, mysql, php">
+    <meta name="author" content="Albert Ząbkiewicz">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Darmowy shitpost xDDDD</title>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <script src="js/tinymce.js"></script>
+</head>
+<body>
+    <header></header>
+    <main></main>
+    <form method="post">
+        <textarea id="mce"></textarea>
+    </form>
+    <footer></footer>
+</body>
+</html>
