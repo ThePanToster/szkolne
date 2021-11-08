967d588342e6180fb0d41db0d6fbc6f81acf5fcf
ThePanToster <alcrafter@o2.pl>
2021-11-08 15:32:19 +0100

    Big changes

--- PHP/projekt/index.php
+++ PHP/projekt/index.php
@@ -2,22 +2,29 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Projekt na AI :D">
+    <meta name="description" content="AI Project">
     <meta name="keywords" content="html, css, mysql, php">
     <meta name="author" content="Albert Ząbkiewicz">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Darmowy shitpost xDDDD</title>
+    <title>Free shitpost website</title>
     <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="css/index.css">
     <script src="js/tinymce.js"></script>
 </head>
 <body>
-    <header></header>
-    <main></main>
-    <form method="post">
-        <textarea id="mce"></textarea>
-    </form>
+    <header>
+        <h1>PhpMyAnim</h1>
+        <a href="" class="signin">Sign Up</a>
+        <a href="" class="signup">Sign In</a>
+        <!-- <div class="signin"><a href="">Sign Up</a></div>
+        <div class="signup"><a href="">Sign In</a></div> -->
+    </header>
+    <main>
+        <form method="post">
+            <!-- <textarea id="mce"></textarea> -->
+        </form>
+    </main>
     <footer></footer>
 </body>
 </html>
