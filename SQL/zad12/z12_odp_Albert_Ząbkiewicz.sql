22504a8b8dde854e4a51ca46a68ca55613354e34
ThePanToster <alcrafter@o2.pl>
2021-12-07 12:58:49 +0100

    SQL zad 12 WIP

--- SQL/zad12/z12_odp_Albert_Ząbkiewicz.sql
+++ SQL/zad12/z12_odp_Albert_Ząbkiewicz.sql
@@ -21,3 +21,12 @@ SELECT count(*) AS wyzsze FROM pracownicyinfo WHERE wyksztalcenie = 'wyzsze';
 5.
 a)
 CREATE VIEW PracownicyPrzydzialZadaniaProjekty(pracownik, wynagrodzenie, nazwa_projektu, dziedzina_projektu) AS SELECT concat(nazwisko, " ", imie), liczbagodzin*stawka, nazwa, dziedzina FROM pracownicy LEFT JOIN przydzial ON pracownicy.nrident = przydzial.nrident JOIN zadaniaprojekty ON przydzial.symbol = zadaniaprojekty.symbol;
+
+b)
+SELECT nazwa_projektu FROM pracownicyprzydzialzadaniaprojekty WHERE pracownik = 'Abacki Jan';
+
+c)
+SELECT sum(wynagrodzenie) FROM pracownicyprzydzialzadaniaprojekty;
+
+d)
+SELECT count(*) FROM pracownicyprzydzialzadaniaprojekty WHERE nazwa_projektu = 'Konserwacja i modernizacja systemu informacyjnego';
